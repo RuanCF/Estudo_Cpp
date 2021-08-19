7df513e9155c1b57419fa36fbea8e15f2f20197b
RuanCF <darkcraft399@gmail.com>
2021-08-18 22:16:39 -0300

    2

---
+++ ManipularVar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        int teste = 10000;
+        //Setagem da variavel
+        cout << endl;
+        //O codigo acima quebra uma linha 
+        cout << "teste de manipulacao de variavel: " << teste << endl;
+        //O endl funciona como o \n, As proximas linhas não serão impressas até o endl ser executado
+        cout << endl;
+        //O codigo acima quebra uma linha 
+        cout << "Memoria da Variavel teste: " << sizeof(teste) << " Bytes" << endl;
+        //sizeof te entrega o valor de bytes que uma variavel esta ocupando     
+        cout << endl;
+        //A linha a cima quebra uma linha
+        system("pause");
+        //Linha a cima pausa o terminal antes de encerrar o programa
+}

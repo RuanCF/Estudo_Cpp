3a150cdf002e66a1eafe1fd59fe90f7f78451a2f
RuanCF <darkcraft399@gmail.com>
2021-08-18 22:17:07 -0300

    003

---
+++ Stream.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    cout << "Estudo de entrada e saida de dados." << endl;
+
+    cout << setw(10) << 1234 << endl;
+    cout << setw(10) << 65432135 << endl;
+    cout << setw(10) << 7423194 << endl;
+    cout << setw(10) << 38220 << endl;
+    system("pause");
+    return 0;
+}

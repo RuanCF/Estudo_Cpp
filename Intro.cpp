c6843b96631d0fd6a336d23149696e65b2c53483
RuanCF <darkcraft399@gmail.com>
2021-08-18 22:15:55 -0300

    001

---
+++ Intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    cout << "\n\nHello World!\n\n"; 
+    //Count é um acrônimo de Console Output
+    //o operador de inserção ( << ) esta dizendo que o texto a direita deve ser "inserido" no objeto cout. 
+    //o operador << faz com que o count receba o valor dentro do " " 
+    system("pause"); 
+    //faz com que a aplicacao entre em pausa antes de finalizar
+    //se nao mandar a aplicacao para, não seria possível ver o que foi impresso no Console.
+} 
